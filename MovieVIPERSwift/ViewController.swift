a1447ccf73aa56e6d1477af5a80d52fbe7982dbb
KevinhoMorales <kfmorales94@gmail.com>
2023-02-05 21:52:07 -0500

    Project Init

---
+++ MovieVIPERSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieVIPERSwift
+//
+//  Created by Kevin Morales on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
